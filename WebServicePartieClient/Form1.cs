12b5da6e85b838e87d1a9410032eada9cf9eb496
fatousow1997 <86850873+fatousow1997@users.noreply.github.com>
2022-05-30 03:47:59 +0100

    page Acceuil

--- WebServicePartieClient/Form1.cs
+++ WebServicePartieClient/Form1.cs
@@ -17,5 +17,19 @@ namespace WebServicePartieClient
             InitializeComponent();
         }
 
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
