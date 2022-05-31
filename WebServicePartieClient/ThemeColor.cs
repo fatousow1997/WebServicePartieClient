1759d6890044567130386f632403fbecc1a6db6c
fatousow1997 <86850873+fatousow1997@users.noreply.github.com>
2022-05-31 11:29:13 +0100

    okk

---
+++ WebServicePartieClient/ThemeColor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServicePartieClient
+{
+    public static class ThemeColor
+
+    {
+       public static List<string> Colorist = new List<string>() { "#CD5C5C", "#F08080" };
+
+    }
+}
